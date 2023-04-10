2eb557e20b064d7ccbb12dc0fd98fd8ac687005a
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-04-09 21:35:46 -0300

    Requirement: post sing-up page: ok. Next step: hub avaliator

--- src/app.js
+++ src/app.js
@@ -6,8 +6,24 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-const user =[]; // {username, avatar}
-const tweet=[]; //{username, tweet}
+const user = []; // {username, avatar}
+const tweet = []; //{username, tweet}
+
+app.post("/sing-up", (req, res) => {
+    const { username, avatar } = req.body;
+
+    if (!username.trim() || !avatar.trim()) {
+        res.status(404).send("Todos os campos são obrigatórios!");;
+        return;
+    }
+
+    user.push(req.body)
+    res.status(201).send("OK")
+
+
+})
+app.post("/tweet", (req, res) => { })
+app.get("/tweet", (req, res) => { })
 
 
 
